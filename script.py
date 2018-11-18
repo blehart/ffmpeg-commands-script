8c4c9ceac994721be413ea98a10d97ed5a2e029e
Robert Hull <robertthull23@gmail.com>
2018-11-17 23:20:22 -0600

    switch from using argparse to using click

--- script.py
+++ script.py
@@ -4,7 +4,7 @@
 
 import glob
 import json
-import argparse
+import click
 import subprocess
 from os import makedirs, remove
 from os.path import basename, join
@@ -12,97 +12,72 @@ from functools import partial
 from multiprocessing import cpu_count
 from multiprocessing.dummy import Pool
 
-class FloatRange():
-    """Tells the Argument Parser whether the given float is within a certain range."""
-
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-
-    def __contains__(self, other):
-        return self.start <= other <= self.end
-
-    def __getitem__(self, index):
-        if index == 0:
-            return self
-        raise IndexError()
-
-    def __repr__(self):
-        return '{0}-{1}'.format(self.start, self.end)
-
-parser = argparse.ArgumentParser()
-parser.add_argument('path', help='Directory to perform the operation on')
-parser.add_argument('op', choices=['type', 'speed', 'duration', 'chapters', 'concat'], help='Operation to perform')
-parser.add_argument('-s', '--speed', default=2.0, type=float, choices=FloatRange(1.1, 4.0),
-                    help='Specify the change in speed for the change speed operation')
-parser.add_argument('-d', '--duration', default=60, type=int,
-                    help='Specify the duration in minutes for the change duration operation')
-parser.add_argument('-bs', '--batchsize', default=0, type=int,
-                    help='Specify how many files you want to concat together at a time')
-
 FFMPEG_TEMPLATE = 'ffmpeg -i "{0}" {1} "{2}"'
 FFPROBE_TEMPLATE = 'ffprobe -i "{0}" {1}'
 
-def handle_operation(args):
-    """Decide which operation to call based on CL arguments."""
-    new_path = directory_setup(args.path)
-    if args.op == 'type' or args.op == 'speed':
-        convert_(args.op, args.path, new_path, args.speed)
-    elif args.op == 'duration':
-        convert_duration(args.path, new_path, args.duration * 60)
-    elif args.op == 'chapters':
-        split_chapters(args.path, new_path)
-    elif args.op == 'concat':
-        concat_files(args.path, new_path, args.batchsize)
+@click.group()
+@click.argument('path', type=click.Path(exists=True))
+@click.pass_context
+def cli(ctx, path):
+    ctx.obj = (path, directory_setup(path))
 
 
 def directory_setup(path):
     """Create directory to put the new files in."""
-    new_path = 'new' + path
-    i = 2
+    i = 1
     while True:
-        print(new_path)
+        new_path = path + str(i)
         try:
             makedirs(new_path)
             break
         except:
-            new_path = 'new' + str(i) + path
             i += 1
     return new_path
 
 
-def convert_(op, old_path, new_path, speed_a):
-    """Perform the given op on files.
+@cli.command(name='speed')
+@click.option('-s', '--speed', default=2.0, show_default=True, type=click.FloatRange(1.1, 4.0))
+@click.pass_obj
+def convert_speed(paths, speed):
+    if speed > 2.0:
+        speed_b = speed / 2
+        speed = 2.0
+    else:
+        speed_b = 1.0
 
-    Speed - Multiple the speed of mp3 files by speed_a
-    Type - Convert m4a files to mp3 files
-    """
-    if op == 'speed':
-        if speed_a > 2.0:
-            speed_b = speed_a / 2
-            speed_a = 2.0
-        else:
-            speed_b = 1.0
+    input_file_type = '*.mp3'
+    output_file_func = lambda input_f: join(paths[1], basename(input_f))
+    parameters = '-filter:a "atempo={0},atempo={1}" -c:a libmp3lame -q:a 4'.format(speed, speed_b)
 
-        input_file_type = '*.mp3'
-        output_file_func = lambda input_f: join(new_path, basename(input_f))
-        parameters = '-filter:a "atempo={0},atempo={1}" -c:a libmp3lame -q:a 4'.format(speed_a, speed_b)
-    elif op == 'type':
-        input_file_type = '*.m4a'
-        output_file_func = lambda input_f: join(new_path, basename(input_f).replace('.m4a', '.mp3'))
-        parameters = '-b:a 192k -vn'
+    convert_(paths[0], input_file_type, output_file_func, parameters)
 
+
+@cli.command(name='type')
+@click.pass_obj
+def convert_type(paths):
+    input_file_type = '*.m4a'
+    output_file_func = lambda input_f: join(paths[1], basename(input_f).replace('.m4a', '.mp3'))
+    parameters = '-b:a 192k -vn'
+
+    convert_(paths[0], input_file_type, output_file_func, parameters)
+
+
+def convert_(old_path, input_file_type, output_file_func, parameters):
     commands = []
     for input_file in glob.glob(join(old_path, input_file_type)):
         output_file = output_file_func(input_file)
         commands.append(FFMPEG_TEMPLATE.format(input_file, parameters, output_file))
 
-    run_commands(commands)
+    _run_commands(commands)
 
 
-def convert_duration(old_path, new_path, duration):
+@cli.command(name='duration')
+@click.option('-d', '--duration', default=60, show_default=True, type=int, help='Specify the duration in minutes for the change duration operation')
+@click.pass_obj
+def convert_duration(paths, duration):
     """Concat mp3 files and then split into files of duration $duration."""
-    files = sorted(glob.glob(join(old_path, '*.mp3')))
+    duration = duration * 60
+    files = sorted(glob.glob(join(paths[0], '*.mp3')))
     temp_file = join(new_path, 'temp.mp3')
     _concat_files([(files, temp_file)])
     
@@ -116,7 +91,7 @@ def convert_duration(old_path, new_path, duration):
     for i in range(num_files):
         if i == num_files - 1:
             duration += total_duration % duration
-        output_file = join(new_path, 'output{0}.mp3'.format(i))
+        output_file = join(paths[1], 'output{0}.mp3'.format(i))
         start_duration_filename.append((start, duration, output_file))
         start += duration
 
@@ -124,8 +99,10 @@ def convert_duration(old_path, new_path, duration):
     remove(temp_file)
 
 
-def split_chapters(old_path, new_path):
-    filename = glob.glob(old_path + '/*')[0]
+@cli.command(name='chapters')
+@click.pass_obj
+def split_chapters(paths):
+    filename = glob.glob(paths[0] + '/*')[0]
     parameters = '-print_format json -show_chapters -loglevel error'
     completed_process = subprocess.run(FFPROBE_TEMPLATE.format(filename, parameters), stdout=subprocess.PIPE, shell=True)
     chapters_info = json.loads(completed_process.stdout)['chapters']
@@ -133,14 +110,17 @@ def split_chapters(old_path, new_path):
     for entry in chapters_info:
         start_time = float(entry['start_time'])
         duration = float(entry['end_time']) - start_time
-        output_file = join(new_path, entry['tags']['title'] + '.mp3')
+        output_file = join(paths[1], entry['tags']['title'] + '.mp3')
         start_duration_filename.append((start_time, duration, output_file))
     
     _split_file(filename, start_duration_filename)
 
 
-def concat_files(old_path, new_path, batch_size):
-    files = sorted(glob.glob(join(old_path, '*.mp3')))
+@cli.command(name='concat')
+@click.option('-bs', '--batchsize', default=0, type=int, help='Specify how many files you want to concat together at a time')
+@click.pass_obj
+def concat_files(paths, batch_size):
+    files = sorted(glob.glob(join(paths[0], '*.mp3')))
 
     if batch_size == 0:
         batch_size = len(files)
@@ -149,7 +129,7 @@ def concat_files(old_path, new_path, batch_size):
 
     input_output_list = []
     for i, input_list in enumerate(input_lists):
-        output_file = join(new_path, 'output{0}.mp3'.format(i))
+        output_file = join(paths[1], 'output{0}.mp3'.format(i))
         input_output_list.append((input_list, output_file))
 
     _concat_files(input_output_list)
@@ -161,7 +141,7 @@ def _split_file(path, start_duration_filename):
     for start, duration, output_file in start_duration_filename:
         parameters = '-acodec copy -t {0} -ss {1}'.format(duration, start)
         commands.append(FFMPEG_TEMPLATE.format(path, parameters, output_file))
-    run_commands(commands)
+    _run_commands(commands)
 
 
 def _concat_files(input_output_list):
@@ -171,14 +151,14 @@ def _concat_files(input_output_list):
     for input_list, output_file in input_output_list:
         input_files = 'concat:' + '|'.join(input_list)
         commands.append(FFMPEG_TEMPLATE.format(input_files, parameters, output_file))
-    run_commands(commands)
+    _run_commands(commands)
 
 
-def run_commands(commands):
+def _run_commands(commands):
     """Run a set of command line operations in parallel, cpu_count - 1 at a time."""
     pool = Pool(cpu_count() - 1)
     pool.map(partial(subprocess.run, shell=True), commands)
 
 
 if __name__ == "__main__":
-    handle_operation(parser.parse_args())
+    cli()
